40b8031ec20934b0d25629d5b497e21bea4724ed
olegsxm <olegsxm@yandex.ru>
2018-12-14 07:18:17 +0700

    add api interceptor which adds header and api url

--- src/app/shared/interceptors/api.interceptor.ts
+++ src/app/shared/interceptors/api.interceptor.ts
@@ -2,14 +2,24 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../../environments/environment';
+import {AuthService} from '../services/auth.service';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const request = req.clone({
+    const token = this.authService.getToken();
+
+    let request = req.clone({
       url: `${environment.api}${req.url}`
     });
 
+    if (token) {
+      request = request.clone({
+        headers: req.headers.append('Authorization', `Bearer ${token}`)
+      });
+    }
     return next.handle(request);
   }
 }
